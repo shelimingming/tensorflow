054a5c14892d29c12323d1408f48d9abe3d16d24
JumbooWong <493182574@qq.com>
2018-10-13 09:10:46 +0800

    tensorflow helloworld

---
+++ helloTensorflow.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+marrix1 = tf.constant([[3, 3]])
+marrix2 = tf.constant([[2], [2]])
+
+product = tf.matmul(marrix1, marrix2)
+
+# 第一种，需要手动关闭session
+# sess = tf.Session()
+# result = sess.run(product)
+# print(result)
+# sess.close()
+
+# 第二种，和打开流一样的打开session，不需要关闭
+with tf.Session() as sess:
+    result = sess.run(product)
+    print(result)
